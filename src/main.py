42a16e65be85d2e9d74d90496fe017e200758426
JessFint <45701595+jFinton@users.noreply.github.com>
2024-04-03 04:29:16 -0400

    Added copy_file function for main.py, copying files from static dir to public dir

--- src/main.py
+++ src/main.py
@@ -1,8 +1,35 @@
 from textnode import TextNode
 from htmlnode import HTMLNode, ParentNode, LeafNode
+import os, shutil
+
+file_path_public = "./public"
+file_path_static = "./static"
+
 
 def main():
-    test_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    print(test_node)
+    print("Deleting public directory")
+    if os.path.exists(file_path_public):
+        shutil.rmtree(file_path_public)
+    
+    print("Copying static over to public")
+    copy_files(file_path_static, file_path_public)
+
+
+def copy_files(source_file_path, dest_file_path):
+    if not os.path.exists(source_file_path):
+        raise Exception("Source Path does not exist.")
+    
+    if not os.path.exists(dest_file_path):
+        os.mkdir(dest_file_path)
+    
+    for file in os.listdir(source_file_path):
+        original_file = os.path.join(source_file_path, file)
+        dest_file = os.path.join(dest_file_path, file)
+        print(f"Copying {original_file} towards {dest_file}")
+        if os.path.isfile(original_file):
+            shutil.copy(original_file, dest_file)
+        else:
+            copy_files(original_file, dest_file)
+
 
 main()
